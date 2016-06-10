1de032b06ce0cb7640a2a20c0a7861b4c57545a5
Diego T. Fialho <diego.tfialho@gmail.com>
2016-06-10 17:01:29 -0300

    Teste de service no MainCtrl

--- src/assets/js/controllers/mainController.js
+++ src/assets/js/controllers/mainController.js
@@ -3,11 +3,11 @@
 app.controller('MainCtrl', ['$scope', 'contactService', '$window', function ($scope, contactService, $window) {
 	$scope.contacts = [];
 
-	function loadContatos() {
+	$scope.loadContatos = function() {
 		contactService.getContatos().then(function(res){
 			$scope.contacts = res;
 		});
-	}
+	};
 
 	$scope.deletarContato = function(id) {
 		contactService.deletarContato(
@@ -18,5 +18,5 @@ app.controller('MainCtrl', ['$scope', 'contactService', '$window', function ($sc
 		);
 	};
 
-	loadContatos();
+	$scope.loadContatos();
 }]);
--- src/assets/js/tests/test.js
+++ src/assets/js/tests/test.js
@@ -1,13 +1,30 @@
 "use strict";
 
 describe('MainCtrl', function(){
-	var $rootScope, $scope, $controller, MainCtrl;
+	var $rootScope, $scope, $controller, MainCtrl, contactMock;
 
 	beforeEach(module("AgendaApp"));
 	beforeEach(inject(function(_$rootScope_, _$controller_){
 		$rootScope = _$rootScope_;
 		$scope = $rootScope.$new();
 		$controller = _$controller_;
+
+		contactMock = {
+			getContatos: function(){
+				return ['lorem'];
+			},
+			getContato: function(){
+				return {
+					contact_id: 1,
+					name: 'Diego Teixeira Fialho',
+					email: 'diego.tfialho@gmail.com',
+					phone: '(24) 2280-6445',
+					cellphone: '(24) 98858-8704',
+					cpf: '142.770.907-61',
+					nascimento: '21/04/1990'
+				};
+			}
+		};
 
 		MainCtrl = $controller('MainCtrl', {'$rootScope' : $rootScope, $scope : $scope});
 	}));
@@ -16,8 +33,19 @@ describe('MainCtrl', function(){
 		expect(MainCtrl).toBeDefined();
 	});
 
+	it("Contacts deve iniciar com um array vazio", function(){
+		expect($scope.contacts).toEqual(jasmine.any(Array));
+	});
+
 	it("contactService deve estar definido", inject(function(contactService){
 		expect(contactService).toBeDefined();
+	}));
+
+	it("getContacts deve retornar um array", inject(function($controller){
+		var scope = {};
+		var ctrl = $controller('MainCtrl', {$scope: scope});
+		scope.loadContatos();
+		expect(scope.contacts.length).toEqual(0);
 	}));
 });
 
